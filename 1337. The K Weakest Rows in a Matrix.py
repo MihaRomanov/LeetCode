156d5e22cefa333c787e9dafad213a3d5ec18179
mihanik575 <m9854737384@gmail.com>
2023-09-18 11:27:51 +0300

    1337. The K Weakest Rows in a Matrix

---
+++ 1337. The K Weakest Rows in a Matrix.py
@@ -0,0 +1,15 @@
+#https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/description/
+
+class Solution:
+    def kWeakestRows(self, mat: [int], k: int):
+        power = []
+        for r, row in enumerate(mat):
+            tmp = 0
+            for n in row:
+                if n:
+                    tmp += 1
+                else:
+                    break
+            power.append((tmp, r))
+        heapq.heapify(power)
+        return [heapq.heappop(power)[1] for _ in range(k)]
