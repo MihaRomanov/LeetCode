701c0ad6974808179acfd2ac12bbe562fa884fdc
mihanik575 <m9854737384@gmail.com>
2023-09-25 09:57:01 +0300

    389. Find the Difference

---
+++ 389. Find the Difference.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/find-the-difference/
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        dic = defaultdict(int)
+        for c in s:
+            dic[c] += 1
+        for c in t:
+            dic[c] -= 1
+        for k in dic:
+            if dic[k] != 0:
+                return k
