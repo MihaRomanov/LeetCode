d549c1e8403e24e5a8129e86d325d4f765cb2a26
mihanik575 <m9854737384@gmail.com>
2023-10-01 22:01:23 +0300

    132 557 0/10

---
+++ 132 Pattern.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/132-pattern/description/
+
+class Solution(object):
+    def find132pattern(self, nums):
+        if len(nums) < 3:
+            return False
+        curr_min = nums[0]
+        for i in range(1, len(nums)-1):
+            if curr_min < nums[i]:
+                for j in range(i+1, len(nums)):
+                    if curr_min < nums[j] < nums[i]:
+                        return True
+            else:
+                curr_min = nums[i]
+        return False
---
+++ 557. Reverse Words in a String III.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/reverse-words-in-a-string-iii/
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        split_s = s.split()
+
+        answer = []
+
+        for i in range(len(split_s)):
+            reverse_str = list(reversed(split_s[i]))
+            answer.append(''.join(reverse_str))
+        return ' '.join(answer)
