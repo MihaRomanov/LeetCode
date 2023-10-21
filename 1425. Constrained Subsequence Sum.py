59547cdd946295fef98bf8b008c7e32059219313
mihanik575 <m9854737384@gmail.com>
2023-10-21 19:13:55 +0300

    1425 _0/10

---
+++ 1425. Constrained Subsequence Sum.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/constrained-subsequence-sum/description/
+
+class Solution:
+    def constrainedSubsetSum(self, nums: List[int], k: int) -> int:
+        res = nums[0]
+        Dp = [0]*len(nums)
+        Dp[0] = max(0,nums[0])
+        DQ = collections.deque([0])
+
+        for i in range(1, len(nums)):
+            while(len(DQ) > 0 and i - DQ[0] > k):
+                DQ.popleft()
