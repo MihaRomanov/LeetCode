e9579199bffd4588d911963776ed2b403f55a807
mihanik575 <m9854737384@gmail.com>
2023-10-31 15:43:55 +0300

    _0/10

---
+++ 2433. Find The Original Array of Prefix Xor.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/find-the-original-array-of-prefix-xor/description/
+
+
+class Solution:
+    def findArray(self, pref: List[int]) -> List[int]:
+        return [pref[0]] + [pref[i] ^ pref[i-1] for i in range(1, len(pref))]
