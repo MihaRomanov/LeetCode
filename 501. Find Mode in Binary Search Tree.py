aaf130d6e9c25186c297d650ce4b1a8967e7ef02
mihanik575 <m9854737384@gmail.com>
2023-11-01 12:33:57 +0300

    _1/10 +15

---
+++ 501. Find Mode in Binary Search Tree.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/find-mode-in-binary-search-tree/description/
+
+
+class Solution:
+    def findMode(self, root: Optional[TreeNode]) -> List[int]:
+        self.inorderr = []
+        def inorder(root):
+            if not root: return
+            self.inorderr.append(root.val)
+            inorder(root.left)
+            inorder(root.right)
+        inorder(root)
+        freq = collections.Counter(self.inorderr)
+        maxx = max(freq.values())
+        return [x for x in freq if freq[x] == maxx]
