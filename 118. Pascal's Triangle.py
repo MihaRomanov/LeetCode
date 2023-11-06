b492be84f414fa6060c6626125f9d84daa6ff1e5
mihanik575 <m9854737384@gmail.com>
2023-11-06 19:10:13 +0300

    _

--- 118. Pascal's Triangle.py
+++ 118. Pascal's Triangle.py
@@ -1,15 +1,16 @@
 #https://leetcode.com/problems/pascals-triangle/
 
-# class Solution:
-def generate(numRows):
-    triangles = []
-    for i in range(numRows):
-        triangles.append([])
-        for j in range(i + 1):
-            if j == 0 or j == i:
-                triangles[i].append(1)
-            else:
-                triangles[i].append(triangles[i - 1][j - 1] + triangles[i - 1][j])
-    return triangles
 
-print(generate(2))
+class Solution:
+    def generate(numRows):
+        triangles = []
+        for i in range(numRows):
+            triangles.append([])
+            for j in range(i + 1):
+                if j == 0 or j == i:
+                    triangles[i].append(1)
+                else:
+                    triangles[i].append(triangles[i - 1][j - 1] + triangles[i - 1][j])
+        return triangles
+
+    print(generate(2))
