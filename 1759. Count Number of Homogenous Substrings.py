0821995feb89bcf3ebf91278cce9bb3ca63b2727
mihanik575 <m9854737384@gmail.com>
2023-11-09 18:48:02 +0300

    1759 _0/10

---
+++ 1759. Count Number of Homogenous Substrings.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/count-number-of-homogenous-substrings/description/
+
+
+class Solution:
+    def countHomogenous(self, s: str) -> int:
+        res = 0
+        idx = 0
+        mod = 10 ** 9 + 7
+        while idx < len(s) - 1:
+            temp = 1
+            while idx < len(s) - 1 and s[idx] == s[idx + 1]:
+                temp += 1
+                idx += 1
+            res += temp * (temp + 1) // 2
+            idx += 1
+
+        if idx != len(s):
+            res += 1
+        return res % mod
