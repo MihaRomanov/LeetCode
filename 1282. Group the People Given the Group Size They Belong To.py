6beb816359f5ba23ef1097fdab6d0a982b123872
mihanik575 <m9854737384@gmail.com>
2023-11-12 16:41:50 +0300

    _

--- 1282. Group the People Given the Group Size They Belong To.py
+++ 1282. Group the People Given the Group Size They Belong To.py
@@ -6,10 +6,6 @@ import collections
 
 class Solution(object):
     def groupThePeople(self, groupSizes):
-        """
-        :type groupSizes: List[int]
-        :rtype: List[List[int]]
-        """
         groups, result = collections.defaultdict(list), []
         for i, size in enumerate(groupSizes):
             groups[size].append(i)
