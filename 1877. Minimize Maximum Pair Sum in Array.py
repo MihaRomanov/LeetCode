3eb98eb1cfe48c9e807dfca8a964847348552665
mihanik575 <m9854737384@gmail.com>
2023-11-17 19:09:49 +0300

    1877 0/10

---
+++ 1877. Minimize Maximum Pair Sum in Array.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/minimize-maximum-pair-sum-in-array/description/
+
+
+class Solution:
+    def minPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        i = 0
+        j = len(nums) - 1
+        ans = 0
+        while i < j:
+            ans = max(ans, nums[i] + nums[j])
+            i += 1
+            j -= 1
+        return ans
