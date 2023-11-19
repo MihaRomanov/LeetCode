62ee6a9641241bd09841df56769684aaadaaf4c7
mihanik575 <m9854737384@gmail.com>
2023-11-19 17:39:32 +0300

    1887 0/10

---
+++ 1887. Reduction Operations to Make the Array Elements Equal.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/reduction-operations-to-make-the-array-elements-equal/description/
+
+
+class Solution:
+    def reductionOperations(self, nums: List[int]) -> int:
+        n=len(nums)
+        nums.sort(reverse=True)
+        ans=0
+        for i in range(n-1):
+            if nums[i]>nums[i+1]:
+                ans+=i+1
+        return ans
