8f6f852f224f2a5abcfb4407338ce3b8431e72e8
mihanik575 <m9854737384@gmail.com>
2023-11-21 20:30:20 +0300

    _

--- 229. Majority Element II.py
+++ 229. Majority Element II.py
@@ -1,16 +1,14 @@
+
+
+
 class Solution(object):
     def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
         candidate1 = 0
-        candidate2 = 1  # should be different from candidate1
+        candidate2 = 1
         count1 = 0
         count2 = 0
 
         for n in nums:
-            # 判断顺序很重要
             if candidate1 == n:
                 count1 += 1
             elif candidate2 == n:
@@ -22,5 +20,4 @@ class Solution(object):
             else:
                 count1 -= 1
                 count2 -= 1
-
         return [n for n in (candidate1, candidate2) if nums.count(n) > len(nums) // 3]
