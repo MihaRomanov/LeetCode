a3ca959b29561d87b54d1fa2b5625200fd6f7cf8
mihanik575 <m9854737384@gmail.com>
2023-11-24 22:42:01 +0300

    1561 _0/10

---
+++ 1561. Maximum Number of Coins You Can Get.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/maximum-number-of-coins-you-can-get/description/
+
+
+class Solution(object):
+    def maxCoins(self, piles):
+        piles.sort()
+        ans = 0
+        n = len(piles)
+
+        for i in range(n // 3, n, 2):
+            ans += piles[i]
+
+        return ans
