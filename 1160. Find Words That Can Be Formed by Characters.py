b3734fec65933ad13c37f978b88099a083853833
mihanik575 <m9854737384@gmail.com>
2023-12-02 18:18:10 +0300

    1660 _0/10

---
+++ 1160. Find Words That Can Be Formed by Characters.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/description/
+
+
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        result = []
+        for i in words:
+            for j in i:
+                if chars.count(j) < i.count(j):
+                    break
+            else:
+                result.append(len(i))
+        return sum(result)
