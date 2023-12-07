2bfc32c84bd0cd16b7f379a1f70696bbd2bfed2b
mihanik575 <m9854737384@gmail.com>
2023-12-07 16:16:56 +0300

    1903 _0/10

---
+++ 1903. Largest Odd Number in String.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/largest-odd-number-in-string/description/
+
+
+class Solution:
+    def largestOddNumber(self, num: str) -> str:
+        for i in range(len(num) - 1, -1, -1):
+            if int(num[i]) % 2 != 0:
+                return num[:i + 1]
+
+        return ""
