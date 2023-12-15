4366edebe7471e14b40bac212ded954fdf64a4a3
mihanik575 <m9854737384@gmail.com>
2023-12-15 15:40:54 +0300

    12 _6/10

--- 12. Integer to Roman.py
+++ 12. Integer to Roman.py
@@ -1,17 +1,32 @@
 # https://leetcode.com/problems/integer-to-roman/description/
 
 
-class Solution {
-public:
-    string intToRoman(int num) {
-    string ones[] = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-        string
-        tens[] = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-        string
-        hrns[] = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-        string
-        ths[] = {"", "M", "MM", "MMM"};
-
-        return ths[num / 1000] + hrns[(num % 1000) / 100] + tens[(num % 100) / 10] + ones[num % 10];
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        romans = {
+            1: 'I',
+            4: 'IV',
+            5: 'V',
+            9: 'IX',
+            10: 'X',
+            40: 'XL',
+            50: 'L',
+            90: 'XC',
+            100: 'C',
+            400: 'CD',
+            500: 'D',
+            900: 'CM',
+            1000: 'M'
         }
-};
+
+        sortedKeys = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        result = ""
+        while num > 0:
+            print(num)
+            for key in sortedKeys:
+                if num >= key:
+                    quo = num // key
+                    result += romans[key] * quo
+                    num = num % key
+
+        return result
