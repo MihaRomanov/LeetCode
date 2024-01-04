9fbb0f2ad20f932c2ead31a39137b2d27103c67d
mihanik575 <m9854737384@gmail.com>
2024-01-04 21:45:38 +0300

    2870 _0/10

---
+++ 2870. Minimum Number of Operations to Make Array Empty.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/minimum-number-of-operations-to-make-array-empty/
+
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        mp = defaultdict(int)
+        for i in nums:
+            mp[i] += 1
+        ans = 0
+        for i in mp.values():
+            if i == 1:
+                return -1
+            ans += (i + 2) // 3
+
+        return ans
