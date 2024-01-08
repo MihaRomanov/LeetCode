a408892c61758a34a77d42a86213e92ab46fa4e1
mihanik575 <m9854737384@gmail.com>
2024-01-08 22:34:44 +0300

    938 _0/10

---
+++ 938. Range Sum of BST.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/range-sum-of-bst/
+
+
+class Solution:
+    def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
+        def dfs(node):
+            if not node:
+                return
+            if low <= node.val <= high:
+                self.total_sum += node.val
+                dfs(node.left)
+                dfs(node.right)
+            elif node.val < low:
+                dfs(node.right)
+            elif node.val > high:
+                dfs(node.left)
+
+        self.total_sum = 0
+        dfs(root)
+        return self.total_sum
