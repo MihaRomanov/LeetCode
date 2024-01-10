585879e965122172e2943c04bcedbe255dc558bb
mihanik575 <m9854737384@gmail.com>
2024-01-10 22:37:01 +0300

    2385 _0/10

---
+++ 2385. Amount of Time for Binary Tree to Be Infected.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/amount-of-time-for-binary-tree-to-be-infected/
+
+
+class Solution:
+    def amountOfTime(self, root: Optional[TreeNode], start: int) -> int:
+        graph=defaultdict(list)
+        stack=[(root,None)]
+        while stack:
+            n,p=stack.pop()
+            if p:
+                graph[p.val].append(n.val)
+                graph[n.val].append(p.val)
+            if n.left:
+                stack.append((n.left,n))
+            if n.right:
+                stack.append((n.right,n))
+        ans=-1
+        seen={start}
+        queue=deque([start])
+        while queue:
+            for _ in range(len(queue)):
+                u=queue.popleft()
+                for v in graph[u]:
+                    if v not in seen:
+                        seen.add(v)
+                        queue.append(v)
+            ans+=1
+        return ans
